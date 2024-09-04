fbbda05fb1cdfd99c8277cb0b90678e33b43b7ad
ashimasaini45 <37665854+ashimasaini45@users.noreply.github.com>
2024-09-04 19:07:06 +0530

    Add files via upload

--- TextForm.js
+++ TextForm.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 
 
 export default function TextForm(props) {
+
     const handleUpClick = ()=>{
         let newText = text.toUpperCase();
         setText(newText);
@@ -13,11 +14,25 @@ export default function TextForm(props) {
         setText(newText);
     };
 
+    const handleSenClick = ()=>{
+        let sentences = text.split(/[.!?]+/).filter(Boolean).length;
+        setSentences(sentences);
+    };  
+
+    const  handleClearClick = ()=>{
+        let newText = ""; //no space "" required or else return 1 as default character
+        setText(newText);
+        setSentences(0); // Reset sentence count when clearing text
+    };
+
     const handleOnChange = (event)=>{
         setText(event.target.value);
+        handleSenClick();  // Automatically update sentence count when text changes
 };
+  
 
     const [text, setText] = useState("");
+    const [sentences, setSentences] = useState(0);
   
     return (
         <>
@@ -36,13 +51,18 @@ export default function TextForm(props) {
                 onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-1" 
                 onClick={handleLoClick}>Convert to Uppercase</button>
+               
+                <button className="btn btn-primary mx-1" 
+                onClick={handleClearClick}>Clear text</button>
             </div>
             <div className="container my-2">
                 <h5>Your text summary</h5>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>{0.008 * (text.split(" ").length)} minute reading</p>
+                <p>{text.split(/\s+/).filter(Boolean).length} words and {text.length} characters</p>
+                <p>{0.008 * (text.split(" ").filter(Boolean).length)} minute reading</p>
                 <h5>Preview</h5>
                 <p>{text}</p>
+                <h6>Sentences: {sentences}</h6>
+               
            
             </div>
         </>
